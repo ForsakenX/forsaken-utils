7d08ede420f4523789e4c208fd8df917dba50474
Daniel Aquino <mr.danielaquino@gmail.com>
2009-09-28 17:40:05 +0000

    fixed lua formating. made the header lua as well.
    git-svn-id: svn://fly.thruhere.net/forsaken@1157 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- rtl_reader/rtl_reader.c
+++ rtl_reader/rtl_reader.c
@@ -7,6 +7,7 @@ typedef unsigned short uint16;
 
 int parse( char * file );
 int parse_light( FILE * fp );
+void tab( int count );
 
 #define get( V, S ) \
 	if ( fread( &(V), sizeof( V ), 1, fp ) != 1 ) \
@@ -28,7 +29,7 @@ int parse_light( FILE * fp );
         else\
         {\
 		tab(tabs);\
-                printf(S": '%s',\n",T[s]);\
+                printf(S" = '%s',\n",T[s]);\
         }
 
 #define getf( S ) \
@@ -40,7 +41,7 @@ int parse_light( FILE * fp );
         else\
         {\
 		tab(tabs);\
-                printf(S": %f,\n",f);\
+                printf(S" = %f,\n",f);\
         }
 
 #define gets( S ) \
@@ -52,7 +53,19 @@ int parse_light( FILE * fp );
         else\
         {\
 		tab(tabs);\
-                printf(S": %d,\n",s);\
+                printf(S" = %d,\n",s);\
+        }
+
+#define getb( S ) \
+        if ( fread( &(b), sizeof( uint32 ), 1, fp ) != 1 ) \
+        {\
+                printf("Error reading " S " from file\n");\
+                return 1;\
+        }\
+        else\
+        {\
+		tab(tabs);\
+                printf(S" = %d,\n",b);\
         }
 
 #define tputs( S ) \
@@ -62,6 +75,11 @@ int parse_light( FILE * fp );
 
 int main (int argc, char** argv) 
 {
+	if(argc < 2)
+	{
+		puts("Error: You must pass a file to parse as the first argument!");
+		exit(1);
+	}
 	return parse( argv[1] );
 }
 
@@ -75,11 +93,10 @@ void tab( int count )
 int parse( char * file )
 {
 	FILE 	* fp;
-
-	int	j;
+	int	j, tabs, number_of_lights;
 	char	MagicNumber[4];
-	uint32	VersionNumber;
-	uint16 	number_of_lights;
+	uint16  s;
+	uint32 	b;
 
 	fp = fopen( file, "rb" );
 	if(!fp)
@@ -88,12 +105,14 @@ int parse( char * file )
 		return 1;
 	}
 
-	puts("--[[");
-	printf("parsed file: %s\n", file);
-	get( MagicNumber, " magic number = %.4s" );
-	get( VersionNumber, " version number = %d" );
-	get( number_of_lights, " there are %d lights in the file" );
-	puts("--]]");
+	puts("--[[ lua table format ]]");
+	printf("file = '%s',\n", file);
+	printf("description = 'real time lights',\n");
+
+	get( MagicNumber, "magic_number = '%.4s'," );
+	getb("version_number");
+	gets("total_lights");
+	number_of_lights = s;
 
 	for ( j = 0; j < number_of_lights; j++ )
 	{
@@ -163,7 +182,7 @@ int parse_light( FILE * fp )
 	getf("g");
 	getf("b");
 
-	tputs("generation: {");
+	tputs("generation = {");
 	tabs++;
 	gett("type", generation_types);
 	getf("delay");
@@ -171,7 +190,7 @@ int parse_light( FILE * fp )
 	tputs("}");
 
 	tab(tabs);
-	printf("%s: {\n", light_types[light_type] );
+	printf("%s = {\n", light_types[light_type] );
 	tabs++;
 
 	switch ( light_type )
@@ -203,7 +222,7 @@ int parse_light( FILE * fp )
 		break;
 	case LIGHT_SPOT:
 		
-			tputs("dir: {");
+			tputs("dir = {");
 			tabs++;
 			getf("x");
 			getf("y");
@@ -211,7 +230,7 @@ int parse_light( FILE * fp )
 			tabs--;
 			tputs("}");
 
-			tputs("up: {");
+			tputs("up = {");
 			tabs++;
 			getf("x");
 			getf("y");
