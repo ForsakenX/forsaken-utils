c6df0d8043bd7db91b5541f37cd277b0f1a82ac8
Daniel Aquino <mr.danielaquino@gmail.com>
2012-09-13 06:25:03 +0000

    moving to stdlib based integer types for 64bit system support and updating output format to match the ruby script
    git-svn-id: svn://fly.thruhere.net/forsaken@2444 1361c5d3-c82f-4d94-b015-8aedb5f389d9

--- bsp_tools/parse_bsp_file.c
+++ bsp_tools/parse_bsp_file.c
@@ -1,15 +1,8 @@
-
+#include <stdlib.h>
 #include <stdio.h>
 
 #define BSP_VERSION_NUMBER      (1)
 #define MAGIC_NUMBER    0x584A5250 //1481265744
-
-typedef	unsigned char		uint8;			/* Unsigned 8 Bit Integer  */
-typedef	char				int8;			/* Signed 8 Bit Integer    */
-typedef	unsigned short int	uint16;			/* Unsigned 16 Bit Integer */
-typedef	short int			int16;			/* Signed 16 bit Integer   */
-typedef	unsigned long		uint32;			/* Unsigned 32 Bit Integer */
-typedef	long				int32;			/* Signed 32 Bit Integer   */
 
 // Represents a vector
 typedef struct VECTOR {
@@ -31,69 +24,76 @@ typedef struct BSP_RAWNODE
 
 // parse the bsp file
 
-int parse_bsp ( char * filename ) {
-
+int parse_bsp ( char * filename )
+{
   FILE * fp;
-  uint32 MagicNumber, VersionNumber;
-  int16	NumGroups, NumNodes = 0;
+  u_int32_t MagicNumber, VersionNumber;
+  int16_t	NumGroups, NumNodes = 0;
   int GroupCount, NodeCount = 0;
 
   // open the file
 
   fp = fopen( filename, "rb" );
-  if( fp == NULL ) {
+  if( fp == NULL )
+	{
     printf("Could not open file!\n");
     return 1;
   }
 
   // get the magic number
 
-  fread( &MagicNumber, sizeof( uint32 ), 1, fp );
-  printf("magic number\t = %i\n",MagicNumber);
-  if( MagicNumber != MAGIC_NUMBER ) {
+  fread( &MagicNumber, sizeof( u_int32_t ), 1, fp );
+  printf("magic number\t = %d\n",MagicNumber);
+  if( MagicNumber != MAGIC_NUMBER )
+	{
     printf("Notice: Magic number not correctly set...\n");
   }
 
   // get the bsp version number
 
-  fread( &VersionNumber, sizeof( uint32 ), 1, fp );
-  printf("pic version\t = %i\n",VersionNumber);
-  if( VersionNumber != BSP_VERSION_NUMBER ){
+  fread( &VersionNumber, sizeof( u_int32_t ), 1, fp );
+  printf("pic version\t = %d\n",VersionNumber);
+  if( VersionNumber != BSP_VERSION_NUMBER )
+	{
     printf("Notice: Pic Version Number not correctly set...\n");
   }
   
   // get number of groups
 
-  fread( &NumGroups, sizeof( int16 ), 1, fp );
+  fread( &NumGroups, sizeof( int16_t ), 1, fp );
   printf("Number of Groups\t = %i\n",NumGroups);
 
   // for each group
 
-  for( GroupCount = 0; GroupCount < NumGroups; GroupCount++ ) {
-    printf("\n\n");
+  for( GroupCount = 0; GroupCount < NumGroups; GroupCount++ )
+	{
+    fread( &NumNodes, sizeof( int16_t ), 1, fp );
 
-    printf("Group:\t %i\n",GroupCount);
-
-    fread( &NumNodes, sizeof( int16 ), 1, fp );
-    printf("Nodes:\t %i\n",NumNodes);
+    printf("group: %i, nodes: %i\n",
+			GroupCount, NumNodes);
 
     // for each node
     
-    for( NodeCount = 0; NodeCount < NumNodes; NodeCount++ ){
+    for( NodeCount = 0; NodeCount < NumNodes; NodeCount++ )
+		{
       BSP_RAWNODE Node;
       VECTOR vector;
 
-      printf("\n");
-      printf("Node:\t %i\n",NodeCount);
-
       fread( &Node, sizeof( BSP_RAWNODE ), 1, fp );
-
       vector = (VECTOR) Node.Normal;
 
-      printf("x (%f), y (%f), z (%f)\n", vector.x, vector.y, vector.z);
+      printf(
+				"Node: %i, "
+				"x: %f, y: %f, z: %f, "
+				"offset: %f, front: %i, back: %i, "
+				"color: %i"
+				"\n",
+				NodeCount,
+				vector.x, vector.y, vector.z,
+        Node.Offset, Node.Front, Node.Back,
+				Node.Colour
+			);
 
-      printf("Offset (%f), Front (%i), Back (%i), Colour (%i)\n",
-             Node.Offset,  Node.Front,  Node.Back,  Node.Colour);
     }
 
 	}
@@ -101,8 +101,10 @@ int parse_bsp ( char * filename ) {
   return 0;
 }
 
-int main( int argc , char *argv[] ){
-  if ( argc < 2 ) {
+int main( int argc , char *argv[] )
+{
+  if ( argc < 2 )
+	{
     printf("Usage: ./%s <filename>\n",argv[0]);
     return 1;
   }
